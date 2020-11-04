d41d55dbb6ae55e0c3b1afec0c46146da16d6df5
zahidfahad <zh.fahad123@gmail.com>
2020-11-04 18:31:15 +0600

    added comments

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import redirect
 from .forms import PostForm
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 # @login_required
 def post_list(request):
@@ -62,3 +63,29 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
